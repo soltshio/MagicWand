61d78e7e3db976d98db5668a0e42eebe417fb311
soltshio <shanshanshien@gmail.com>
2026-03-25 21:12:36 +0900

    新しい操作の作成の準備

---
+++ MagicWand/Assets/Script/WandControl/Component/ver3/WandControllerVer3.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+//作成者:杉山
+//杖を動かすクラス(バージョン3)
+//常にジャイロオン
+//ひっくり返っているかは考慮しない
+//回転速度を調整可能
+
+public class WandControllerVer3 : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
