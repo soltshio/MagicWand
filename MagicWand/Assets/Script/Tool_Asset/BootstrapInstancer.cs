8e358a597eaac30f0fd6eb51335e36af08990516
soltshio <shanshanshien@gmail.com>
2026-05-11 15:33:21 +0900

    起動時にResources/Bootstrap内のオブジェクトを自動で生成するようにした

---
+++ MagicWand/Assets/Script/Tool_Asset/BootstrapInstancer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+//作成者:杉山
+//起動時にResources/Bootstrapに入っているプレハブを全てインスタンス化する
+
+public class BootstrapInstancer
+{
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    private static void Initialize()
+    {
+        var bootstrapPrefabs = Resources.LoadAll<GameObject>("Bootstrap");
+
+        foreach (var prefab in bootstrapPrefabs)
+        {
+            GameObject obj = GameObject.Instantiate(prefab);
+            Object.DontDestroyOnLoad(obj);
+        }
+    }
+}
