93dca69ec0f12dfe9d636db2c58945d1cb690cef
soltshio <shanshanshien@gmail.com>
2026-05-12 14:15:43 +0900

    マウスで操作できるようにする作業(まだ途中)

---
+++ MagicWand/Assets/Script/MagicCircle/Component/MagicSphere/MagicSphereVer3.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+//作成者：杉山
+//魔法陣上の球
+
+public class MagicSphereVer3 : MonoBehaviour
+{
+    [SerializeField]
+    Renderer _renderer;
+
+    [SerializeField]
+    Material _deactiveMat;
+
+    bool _isActive;
+
+    public bool IsActive { get => _isActive; }
+
+    public void ToActive(Material material)
+    {
+        if (_isActive == true) return;
+        
+        _isActive = true;
+        _renderer.material = material;
+    }
+
+    public void ToDeactive()
+    {
+        if (_isActive == false) return;
+        
+        _isActive = false;
+        _renderer.material = _deactiveMat;
+    }
+
+    void Awake()
+    {
+        _isActive = false;
+        _renderer.material = _deactiveMat;
+    }
+
+}
---
+++ MagicWand/Assets/Script/WandControl/Component/MagicCircleTracerCursor.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+//作成者：杉山
+//魔法陣をなぞるカーソルの動き
+//レイを飛ばして、魔法陣上の球との当たり判定をとる
+
+public class MagicCircleTracerCursor : MonoBehaviour
+{
+    void Update()
+    {
+        Vector2 mousePos = Mouse.current.position.ReadValue();
+
+        Ray ray = Camera.main.ScreenPointToRay(mousePos);
+
+        RaycastHit hit;
+
+        if (!Physics.Raycast(ray, out hit)) return;
+
+        if (!hit.collider.CompareTag(TagNameList.MagicSphere)) return;
+
+        var magicSphere = hit.collider.GetComponent<MagicSphereVer3>();
+        magicSphere.ToDeactive();
+    }
+}
