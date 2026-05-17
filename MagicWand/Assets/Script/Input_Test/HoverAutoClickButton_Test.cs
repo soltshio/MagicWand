0cbef689525fc2e56f4f679121f1ebc628ac8ed7
soltshio <shanshanshien@gmail.com>
2026-05-17 22:26:10 +0900

    どのくらいの時間、カーソルを合わせているのかを可視化するようにした。

--- MagicWand/Assets/Script/Input_Test/HoverAutoClickButton_Test.cs
+++ MagicWand/Assets/Script/Input_Test/HoverAutoClickButton_Test.cs
@@ -2,6 +2,7 @@
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using System.Collections;
+using System;
 
 //作成者:杉山
 //数秒間カーソルを合わせていると、自動クリックするボタンのテスト
@@ -10,23 +11,30 @@ using System.Collections;
 public class HoverAutoClickButton_Test : MonoBehaviour, IPointerEnterHandler,IPointerExitHandler
 {
     [Tooltip("何秒間カーソルを合わせていると自動クリックされるか")] [SerializeField]
-    private float _hoverTime = 2f;
+    private float _hoverDurationToClick = 2f;
 
     [SerializeField]
     private float _pressDuration = 0.1f;
 
+    public event Action OnAutoClick; // 自動クリックされたときのイベント
+    public event Action OnCursorEnter; // マウスカーソルがボタンに合わさったときのイベント
+    public event Action OnCursorExit; // マウスカーソルがボタンから離れたときのイベント
+
     private bool _isHovering=false;//マウスカーソルがボタンに合わさっているか
     private bool _hasClicked=false;//クリックされたか
-    private float _timer=0f;
+    private float _hoveringTime=0f;//カーソルが合わさっている時間
+
+    public float HoveringTime { get => _hoveringTime; }//カーソルが合わさっている時間
+    public float HoverDurationToClick { get => _hoverDurationToClick; }//何秒間カーソルを合わせていると自動クリックされるか
 
     private void Update()
     {
         if (!_isHovering || _hasClicked) return;
 
 
-        _timer += Time.deltaTime;
+        _hoveringTime += Time.deltaTime;
 
-        if (_timer >= _hoverTime)
+        if (_hoveringTime >= _hoverDurationToClick)
         {
             StartCoroutine(AutoClickCoroutine());
 
@@ -66,14 +74,18 @@ public class HoverAutoClickButton_Test : MonoBehaviour, IPointerEnterHandler,IPo
 
     public void OnPointerEnter(PointerEventData eventData)
     {
+        OnCursorEnter?.Invoke();
+
         _isHovering = true;
-        _timer = 0f;
+        _hoveringTime = 0f;
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
+        OnCursorExit?.Invoke();
+
         _isHovering = false;
         _hasClicked = false;
-        _timer = 0f;
+        _hoveringTime = 0f;
     }
 }
