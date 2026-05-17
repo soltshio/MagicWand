dbe94da6b6f26f02253718190261fb2f03885775
soltshio <shanshanshien@gmail.com>
2026-05-18 00:30:55 +0900

    ステートを作成(実装はまだ)

---
+++ MagicWand/Assets/Script/UI/HoverAutoClickButton/PureC#/State/HoverAutoClickButtonStateTypeHovering.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+//作成者:杉山
+//数秒間カーソルを合わせていると、自動クリックするボタンのカーソルが合っている状態の振る舞い
+
+public class HoverAutoClickButtonStateTypeHovering : HoverAutoClickButtonStateTypeBase
+{
+    public void OnEnter(HoverAutoClickButtonStateMachine stateMachine)
+    {
+
+    }
+
+    public void OnUpdate(HoverAutoClickButtonStateMachine stateMachine)
+    {
+
+    }
+
+    public void OnExit(HoverAutoClickButtonStateMachine stateMachine)
+    {
+
+    }
+}
