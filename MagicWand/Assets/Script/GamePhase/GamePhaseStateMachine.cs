c604e419de345532f4bb117d7b2b5a412f1a9a52
soltshio <shanshanshien@gmail.com>
2026-05-25 17:28:24 +0900

    ステートマシンに処理追加

--- MagicWand/Assets/Script/GamePhase/GamePhaseStateMachine.cs
+++ MagicWand/Assets/Script/GamePhase/GamePhaseStateMachine.cs
@@ -5,10 +5,25 @@
 
 public class GamePhaseStateMachine : MonoBehaviour
 {
-    
+    [SerializeField]
+    SerializableDictionary<EGamePhaseState, GamePhaseStateTypeBase> _stateDictionary;
+
+    [Tooltip("最初のステート")] [SerializeField]
+    EGamePhaseState _initialState=EGamePhaseState.None;
+
+    GamePhaseStateTypeBase _currentState;
 
     void Awake()
     {
 
+    }
+
+    public void ChangeState(EGamePhaseState nextState)
+    {
+        if(_currentState != null) _currentState.OnExit(this);
+
+        _currentState = _stateDictionary[nextState];
+
+        if(_currentState != null) _currentState.OnEnter(this);
     }
 }
