acb1f70a7d17d233a6ec8ed8970654919a7f8330
soltshio <shanshanshien@gmail.com>
2026-06-04 22:30:19 +0900

    クリアシーンでフェードインが入るようにした

---
+++ MagicWand/Assets/Script/UI/FadeInOutPanel/Component/FadeInTriggerOnStart.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
+//作成者:杉山
+//シーン開始時にフェードインを開始する
+
+public class FadeInTriggerOnStart : MonoBehaviour
+{
+    [SerializeField] 
+    FadeInOutPanel _fadeInOutPanel;
+
+    void Start()
+    {
+        _fadeInOutPanel.FadeTrigger(FadeInOutPanel.FadeEType.FadeIn);
+    }
+}
