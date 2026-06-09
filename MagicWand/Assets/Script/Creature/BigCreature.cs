26ddedceea2bd1ac3befec586fc64a18a0c12d92
soltshio <shanshanshien@gmail.com>
2026-06-09 17:19:05 +0900

    でか生物が動いてからゲームが終わるようになった

--- MagicWand/Assets/Script/Creature/BigCreature.cs
+++ MagicWand/Assets/Script/Creature/BigCreature.cs
@@ -1,5 +1,6 @@
 ﻿using Cysharp.Threading.Tasks;
 using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 //作成者:杉山
@@ -9,6 +10,12 @@ public class BigCreature : MonoBehaviour
 {
     [Tooltip("最大体力(起きるまでに対象の魔法を撃たないといけない回数)")] [SerializeField]
     int _maxHp;
+
+    [SerializeField]
+    float _moveDuration;
+
+    [SerializeField]
+    Transform _destination;
 
     [SerializeField]
     AudioSource _audioSource;
@@ -46,10 +53,35 @@ public class BigCreature : MonoBehaviour
             //zzz音
         }
 
+        await UniTask.Delay(TimeSpan.FromSeconds(1f));
+
         //体力が0になったら起きて道を譲る演出を入れる
         if (!_isWakeUp) return;
 
         //起きた効果音
         //移動
+        Vector3 beforeMovePos = transform.position;
+
+        float elapsed = 0f;
+
+        //進行方向に向かせとく
+        Vector3 moveDirection = _destination.position - beforeMovePos;
+        Quaternion lookRot = Quaternion.LookRotation(moveDirection, Vector3.up);
+        transform.rotation = lookRot;
+
+        while(true)
+        {
+            elapsed += Time.deltaTime;
+
+            float rate = elapsed / _moveDuration;
+
+            transform.position = Vector3.Lerp(beforeMovePos, _destination.position, rate);
+
+            if (elapsed >= _moveDuration) break;
+
+            await UniTask.Yield(PlayerLoopTiming.Update,cancellationToken:token);
+        }
+
+        transform.position = _destination.position;
     }
 }
--- MagicWand/Assets/Script/GamePhase/Component/InGameScene/GamePhaseStateTypeGame_InGameScene.cs
+++ MagicWand/Assets/Script/GamePhase/Component/InGameScene/GamePhaseStateTypeGame_InGameScene.cs
@@ -11,17 +11,17 @@ using UnityEngine;
 public class GamePhaseStateTypeGame_InGameScene : GamePhaseStateTypeBase
 {
     [SerializeField]
-    int _clearCount = 3;
-
-    [SerializeField]
     MagicCircleManagerVer3 _magicCircleManager;
 
     [SerializeField]
     MagicInvoker _magicInvoker;
 
+    [SerializeField]
+    BigCreature _bigCreature;
+
     public override void OnEnter(GamePhaseStateMachine stateMachine)
     {
-        GameStateAsync(stateMachine).Forget();
+        GameStateAsync(stateMachine,this.GetCancellationTokenOnDestroy()).Forget();
     }
 
     public override void OnUpdate(GamePhaseStateMachine stateMachine)
@@ -34,15 +34,21 @@ public class GamePhaseStateTypeGame_InGameScene : GamePhaseStateTypeBase
         
     }
 
-    async UniTask GameStateAsync(GamePhaseStateMachine stateMachine)
+    async UniTask GameStateAsync(GamePhaseStateMachine stateMachine,CancellationToken token)
     {
-        for(int i=0; i<_clearCount ;i++)
+        while(true)
         {
             //魔法陣を起動
             var invokableMagics = await _magicCircleManager.MagicCircleAsync();
 
             //魔法を発動
             await _magicInvoker.InvokeMagicAsync(invokableMagics);
+
+            //一応少しだけ待つ
+            await UniTask.Delay(TimeSpan.FromSeconds(1f), cancellationToken: token);
+
+            //でか生物がどいたらゲーム終了
+            if (_bigCreature._isWakeUp) break;
         }
 
         stateMachine.ChangeState(EGamePhaseState.Finish);
