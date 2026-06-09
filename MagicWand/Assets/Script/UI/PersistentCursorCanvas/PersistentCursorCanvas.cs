350c02a732d6a375a80f6469bcb24fed93296f13
soltshio <shanshanshien@gmail.com>
2026-06-09 19:49:50 +0900

    常にカーソルエフェクトが見えるようにした

---
+++ MagicWand/Assets/Script/UI/PersistentCursorCanvas/PersistentCursorCanvas.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.SceneManagement;
+
+//作成者:杉山
+//シーンが変わるごとにキャンバスのrenderCameraを更新する
+
+public class PersistentCursorCanvas : MonoBehaviour
+{
+    [SerializeField]
+    Canvas _canvas;
+
+    [SerializeField]
+    Camera _uiCameraPrefab;
+
+    private void Start()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+
+        var uiCamera = InitUICamera();
+
+        if (uiCamera == null) return;
+
+        UpdateRenderCamera(uiCamera);
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        var uiCamera = InitUICamera();
+
+        if (uiCamera == null) return;
+
+        UpdateRenderCamera(uiCamera);
+    }
+
+    Camera InitUICamera()
+    {
+        //UIカメラが無い時は無理やり作り、メインカメラのStackに入れる
+        GameObject uiCameraObj = GameObject.FindWithTag("UICamera");
+
+        if (uiCameraObj == null)
+        {
+            Debug.Log("UICameraタグのオブジェクトが見つかりませんでした");
+            return MakeUICamera();
+        }
+
+        Camera uiCamera = uiCameraObj.GetComponent<Camera>();
+
+        if (uiCamera == null)
+        {
+            Debug.Log("UICameraタグのオブジェクトにCameraが付いていません");
+            return MakeUICamera();
+        }
+
+        return uiCamera;
+    }
+
+    Camera MakeUICamera()
+    {
+        Camera mainCamera = Camera.main;
+
+        if (mainCamera == null)
+        {
+            Debug.LogError("Main Cameraが見つかりません");
+            return null;
+        }
+
+        //UIカメラを作る
+        var uiCamera = Instantiate(_uiCameraPrefab, Vector3.zero, Quaternion.identity);
+
+        var mainData = mainCamera.GetUniversalAdditionalCameraData();
+
+        if (!mainData.cameraStack.Contains(uiCamera))
+        {
+            mainData.cameraStack.Add(uiCamera);
+        }
+
+        return uiCamera;
+    }
+
+    void UpdateRenderCamera(Camera uiCamera)
+    {
+        _canvas.worldCamera = uiCamera;
+    }
+}
