4c018e1349affb20253ee04434adbe21724ad2ce
soltshio <shanshanshien@gmail.com>
2026-06-09 18:02:50 +0900

    クリア画面からタイトル画面に戻れるようになった

---
+++ MagicWand/Assets/Script/Scene/FromClearToTitleSceneLoader.cs
@@ -0,0 +1,32 @@
+﻿using Cysharp.Threading.Tasks;
+using System;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FromClearToTitleSceneLoader : MonoBehaviour
+{
+    [SerializeField]
+    float _delayToLoad = 0.5f;
+
+    bool _isLoading = false;
+
+    public void StartLoad()
+    {
+        //既にロードが始まってたら弾く
+        if (_isLoading) return;
+
+        LoadSceneAsync(this.GetCancellationTokenOnDestroy()).Forget();
+    }
+
+    async UniTask LoadSceneAsync(CancellationToken ct)
+    {
+        _isLoading = true;
+
+        await UniTask.Delay(TimeSpan.FromSeconds(_delayToLoad), cancellationToken: ct);
+
+        await SceneManager.LoadSceneAsync(SceneNameList.TitleScene).ToUniTask(cancellationToken: ct);
+
+        _isLoading = false;
+    }
+}
