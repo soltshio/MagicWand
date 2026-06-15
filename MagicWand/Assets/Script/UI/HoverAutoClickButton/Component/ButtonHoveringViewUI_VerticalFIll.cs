aede58ed876e07f7af074d259feda754c089c9b8
soltshio <shanshanshien@gmail.com>
2026-06-15 15:30:29 +0900

    AudioMixerの作成と設定

---
+++ MagicWand/Assets/Script/UI/HoverAutoClickButton/Component/ButtonHoveringViewUI_VerticalFIll.cs
@@ -0,0 +1,83 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+//作成者:杉山
+//数秒間カーソルを合わせていると、自動クリックするボタンがホバー中(カーソルが合わせられている時)にホバー時間の割合をゲージで表示する
+//文字のマテリアル(VerticalFillTextShader)の_FillAmountと_FillColorを操作して、文字が下から上に色で塗りつぶされていくように見せる
+
+public class ButtonHoveringViewUI_VerticalFIll : MonoBehaviour
+{
+    [SerializeField]
+    HoverAutoClickButton _hoverAutoClickButton;
+
+    [SerializeField]
+    TextMeshProUGUI _buttonText;
+
+    [SerializeField]
+    Color _fillColor;
+
+    bool _isHovering = false;
+
+    Material _material;
+
+    static readonly int FillAmountID = Shader.PropertyToID("_FillAmount");
+
+    static readonly int FillColorID = Shader.PropertyToID("_FillColor");
+
+    void Awake()
+    {
+        // インスタンス化されたマテリアルを取得
+        _material = _buttonText.fontMaterial;
+    }
+
+    void Start()
+    {
+        //色を変えておく
+        _material.SetColor(FillColorID, _fillColor);
+    }
+
+    void OnValidate()
+    {
+        //実行時以外は無視
+        if (!Application.isPlaying) return;
+        
+        if (_buttonText == null) return;
+        if (_material == null) return;
+
+        _material.SetColor(FillColorID, _fillColor);
+    }
+
+    void OnEnable()
+    {
+        _hoverAutoClickButton.OnStateChanged += OnButtonStateChanged;
+    }
+
+    void OnDisable()
+    {
+        _hoverAutoClickButton.OnStateChanged -= OnButtonStateChanged;
+    }
+
+    void Update()
+    {
+        if (!_isHovering) return;
+
+        float colorImageRate = _hoverAutoClickButton.HoveringTime / _hoverAutoClickButton.Parameter.HoverDurationToClick;
+
+        _material.SetFloat(FillAmountID, colorImageRate);
+    }
+
+    void OnButtonStateChanged(HoverAutoClickButtonEState state)
+    {
+        if (state == HoverAutoClickButtonEState.Hovering)
+        {
+            _isHovering = true;
+        }
+        else
+        {
+            _isHovering = false;
+            //色の位置を一番下に戻しておく
+            _material.SetFloat(FillAmountID, 0f);
+        }
+    }
+}
