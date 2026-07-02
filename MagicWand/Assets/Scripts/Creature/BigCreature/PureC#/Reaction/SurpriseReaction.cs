2a762b2794c23afa6db661ecc7fb555a8b9215c1
soltshio <shanshanshien@gmail.com>
2026-07-02 18:11:19 +0900

    ビックリマークのアニメーションを作成

---
+++ MagicWand/Assets/Scripts/Creature/BigCreature/PureC#/Reaction/SurpriseReaction.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+//作成者:杉山
+//ビックリした反応の演出
+
+[System.Serializable]
+public class SurpriseReaction
+{
+    [SerializeField]
+    AudioSource _audioSource;
+
+    [SerializeField]
+    AudioClip _damageSE;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void PlayAudio(AudioClip clip)
+    {
+        _audioSource.clip = clip;
+        _audioSource.Play();
+    }
+}
