118396e0ba3df8170e9972fcf20ef5af4c65dfa0
soltshio <shanshanshien@gmail.com>
2026-07-10 18:28:10 +0900

    TouchDesignerから受け取った値をカーソルの位置に流し込むことに成功

---
+++ MagicWand/Assets/Scripts/Cursor/HokuyoBlobPosReceiver.cs
@@ -0,0 +1,52 @@
+﻿using extOSC;
+using System;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+//作成者:杉山
+//OSC通信で送られてきた北陽レーザーが察知した物体(塊)の座標を受け取る
+
+public class HokuyoBlobPosReceiver : MonoBehaviour
+{
+    [SerializeField]
+    OSCReceiver _oscReceiver;
+
+    private Vector2 _blobPosition = new();
+    const string _posAddressName="/position";
+
+    public event Action<Vector2> OnMovePos;
+
+    public Vector2 BlobPosition
+    {
+        get
+        {
+            return _blobPosition; 
+        } 
+        private set
+        {
+            _blobPosition = value;
+            OnMovePos?.Invoke(_blobPosition);
+        }
+    }
+
+    void Start()
+    {
+        _oscReceiver.Bind(_posAddressName, ReceivePos);
+    }
+
+    void ReceivePos(OSCMessage message)
+    {
+        Vector2 blobPos;
+        blobPos.x = message.Values[0].FloatValue;
+        blobPos.y = message.Values[1].FloatValue;
+
+        BlobPosition = blobPos;
+        
+    }
+
+    void Update()
+    {
+        Debug.Log(BlobPosition);
+    }
+
+}
