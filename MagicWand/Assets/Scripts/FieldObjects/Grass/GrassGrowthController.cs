7e796a29ddc6591f4de13a99e291715a7673f093
soltshio <shanshanshien@gmail.com>
2026-07-29 22:19:55 +0900

    草の成長プログラムの作成(整理は後でします)

--- MagicWand/Assets/Scripts/FieldObjects/Grass/GrassGrowthController.cs
+++ MagicWand/Assets/Scripts/FieldObjects/Grass/GrassGrowthController.cs
@@ -11,9 +11,9 @@ public class GrassGrowthController : MonoBehaviour
     {
         public GameObject leaveObject;
 
-        public float startRate;
+        [Range(0, 1)] public float startRate;
 
-        public float rangeRate;
+        [Range(0, 1)] public float rangeRate;
     }
 
     [SerializeField]
@@ -28,6 +28,8 @@ public class GrassGrowthController : MonoBehaviour
     [SerializeField] [Range(0, 1)]
     float _defaultGrowthRate;
 
+    static readonly int _displayRateID = Shader.PropertyToID("_DisplayRate");
+
     void Start()
     {
         SetGrowth(_defaultGrowthRate);
@@ -38,6 +40,7 @@ public class GrassGrowthController : MonoBehaviour
         SetGrowth(_defaultGrowthRate);
     }
 
+    //1を設定すると最大まで成長させる
     public void SetGrowth(float rate)
     {
         rate = Mathf.Clamp01(rate);
@@ -48,10 +51,42 @@ public class GrassGrowthController : MonoBehaviour
             var growthSegment = _growthSegments[i];
 
             if (growthSegment.leaveObject == null) continue;
+
+            //その成長セグメントに達していない場合は非表示にする
+            if(rate<growthSegment.startRate)
+            {
+                growthSegment.leaveObject.SetActive(false);
+                continue;
+            }
+
+            growthSegment.leaveObject.SetActive(true);
+
+            //位置を設定
+            float heightRate = Mathf.Clamp(rate, growthSegment.startRate, growthSegment.startRate + growthSegment.rangeRate);
+            
+            float leavePosY = heightRate * _height;
+            var leaveLocalPos = growthSegment.leaveObject.transform.localPosition;
+            leaveLocalPos.y = leavePosY;
+            growthSegment.leaveObject.transform.localPosition = leaveLocalPos;
+
+            //大きさを設定
+            float scaleRate = Mathf.InverseLerp(growthSegment.startRate, growthSegment.startRate + growthSegment.rangeRate, rate);
+            scaleRate = Mathf.Clamp01(scaleRate);
+
+            Vector3 leaveScale = new Vector3(scaleRate, scaleRate, scaleRate);
+            growthSegment.leaveObject.transform.localScale=leaveScale;
         }
 
         //茎の成長
+        if (_stemRenderer == null) return;
+
         MaterialPropertyBlock block = new MaterialPropertyBlock();
+
+        _stemRenderer.GetPropertyBlock(block);
+
+        block.SetFloat(_displayRateID, rate);
+
+        _stemRenderer.SetPropertyBlock(block);
     }
 }
 
