923f6a7d614081a99e508a0b92ae444275472ffe
soltshio <shanshanshien@gmail.com>
2026-08-03 00:22:12 +0900

    BigNudiに二度寝モーションをさせるようにした

---
+++ MagicWand/Assets/Scripts/General/PureC#/AnimatorProperty/BigCreatureAnimatorProperty.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+//作成者:杉山
+//巨大生物(BigNudi)のアニメーションのプロパティ
+
+public class BigCreatureAnimatorProperty
+{
+    //起き上がる(二度寝)モーションのブール
+    public static string GoBackToSleepBoolName = "GoBackToSleep";
+}
