a0e5867bd40052035c32954cd4fa6227acdefc57
soltshio <shanshanshien@gmail.com>
2026-08-03 00:50:08 +0900

    一部コードで無駄に重複していた個所を修正

---
+++ MagicWand/Assets/Scripts/SpellCast/SpellCastList.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+//作成者:杉山
+//魔法の種類に対する魔法の詠唱をまとめたリスト
+
+public class SpellCastList : MonoBehaviour
+{
+    [SerializeField]
+    SerializableDictionary<EMagic, SpellCast> _spellCastsDictionary;
+
+    public Dictionary<EMagic, SpellCast> SpellCasts { get { return _spellCastsDictionary; } }
+
+    public bool TryGetSpellCast(EMagic keyMagic,out SpellCast spellCast)
+    {
+        return _spellCastsDictionary.TryGetValue(keyMagic, out spellCast);
+    }
+}
