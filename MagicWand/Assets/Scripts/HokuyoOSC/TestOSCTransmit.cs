81ee92b7efa0fa4b575ff2f82ada3e8bfa4bb56f
soltshio <shanshanshien@gmail.com>
2026-08-17 15:23:18 +0900

    unityから送られてきた値をTouchDesignerで変更するテスト成功

---
+++ MagicWand/Assets/Scripts/HokuyoOSC/TestOSCTransmit.cs
@@ -0,0 +1,23 @@
+﻿using Cysharp.Threading.Tasks;
+using extOSC;
+using UnityEngine;
+
+public class TestOSCTransmit : MonoBehaviour
+{
+    [SerializeField]
+    private OSCTransmitter transmitter;
+
+    [SerializeField]
+    float value;
+
+    async void Start()
+    {
+        await UniTask.Delay(5000,cancellationToken:this.GetCancellationTokenOnDestroy());
+
+        OSCMessage message = new OSCMessage("/test");
+
+        message.AddValue(OSCValue.Float(value));
+
+        transmitter.Send(message);
+    }
+}
