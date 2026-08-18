2e8137070c01aa757c568c22d4501fe2ee596aee
soltshio <shanshanshien@gmail.com>
2026-08-18 19:30:51 +0900

    UIを追加、データごとにタイミングを分けてOSC通信で送れるかをテスト

---
+++ MagicWand/Assets/Scripts/Cursor/CursorAdjust/CursorAdjustSceneManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+//作成者:杉山
+//カーソルの位置を調整するシーンの制御
+
+public class CursorAdjustSceneManager : MonoBehaviour
+{
+    void Start()
+    {
+        //補正開始
+
+        //中心に合わせる
+
+        //画面の中心補正処理を行う
+
+        //右上に合わせる
+        //右下に合わせる
+        //左下に合わせる
+
+        //画面の大きさ補正処理を行う
+
+        //補正終了
+
+        //タイトルシーンに遷移
+    }
+}
--- MagicWand/Assets/Scripts/HokuyoOSC/TestOSCTransmit.cs
+++ MagicWand/Assets/Scripts/HokuyoOSC/TestOSCTransmit.cs
@@ -1,5 +1,6 @@
 ﻿using Cysharp.Threading.Tasks;
 using extOSC;
+using System;
 using UnityEngine;
 
 public class TestOSCTransmit : MonoBehaviour
@@ -10,6 +11,9 @@ public class TestOSCTransmit : MonoBehaviour
     [SerializeField]
     float value;
 
+    [SerializeField]
+    float value2;
+
     async void Start()
     {
         await UniTask.Delay(5000,cancellationToken:this.GetCancellationTokenOnDestroy());
@@ -19,5 +23,13 @@ public class TestOSCTransmit : MonoBehaviour
         message.AddValue(OSCValue.Float(value));
 
         transmitter.Send(message);
+
+        await UniTask.Delay(5000, cancellationToken: this.GetCancellationTokenOnDestroy());
+
+        OSCMessage message2 = new OSCMessage("/ppp");
+
+        message2.AddValue(OSCValue.Float(value2));
+
+        transmitter.Send(message2);
     }
 }
