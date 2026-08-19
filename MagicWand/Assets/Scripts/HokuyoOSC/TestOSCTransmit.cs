b448f63bd8bcd78036457baa20ae1524447b0ee0
soltshio <shanshanshien@gmail.com>
2026-08-19 16:37:08 +0900

    TouchDesigner側のノードを修正

--- MagicWand/Assets/Scripts/HokuyoOSC/TestOSCTransmit.cs
+++ MagicWand/Assets/Scripts/HokuyoOSC/TestOSCTransmit.cs
@@ -8,28 +8,11 @@ public class TestOSCTransmit : MonoBehaviour
     [SerializeField]
     private OSCTransmitter transmitter;
 
-    [SerializeField]
-    float value;
-
-    [SerializeField]
-    float value2;
-
     async void Start()
     {
-        await UniTask.Delay(5000,cancellationToken:this.GetCancellationTokenOnDestroy());
+        var hokuyoDataTransmitter = await HokuyoDataTransmitter.GetInstanceAsync(this.GetCancellationTokenOnDestroy());
 
-        OSCMessage message = new OSCMessage("/test");
-
-        message.AddValue(OSCValue.Float(value));
-
-        transmitter.Send(message);
-
-        await UniTask.Delay(5000, cancellationToken: this.GetCancellationTokenOnDestroy());
-
-        OSCMessage message2 = new OSCMessage("/ppp");
-
-        message2.AddValue(OSCValue.Float(value2));
-
-        transmitter.Send(message2);
+        hokuyoDataTransmitter.SendCenterM(new Vector2(1.0f, 2.0f));
+        hokuyoDataTransmitter.SendSizeM(new Vector2(3.0f, 4.0f));
     }
 }
