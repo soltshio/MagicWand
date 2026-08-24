4b879d44933bd27f68becad9d23d591c133ca6c6
soltshio <shanshanshien@gmail.com>
2026-08-24 23:20:13 +0900

    魔法発動時に誘導エフェクトが非表示になるようにした

--- MagicWand/Assets/Scripts/FieldObjects/Creature/BigCreature/Reaction/BigCreatureSleepPlayer.cs
+++ MagicWand/Assets/Scripts/FieldObjects/Creature/BigCreature/Reaction/BigCreatureSleepPlayer.cs
@@ -87,6 +87,8 @@ public class BigCreatureSleepPlayer : MonoBehaviour
 
     private void OnDisable()
     {
+        _singleTaskCancellation.Cancel();
+
         _bigCreatureStatus.OnUpdateHP -= UpdateShowZCount;
     }
 
