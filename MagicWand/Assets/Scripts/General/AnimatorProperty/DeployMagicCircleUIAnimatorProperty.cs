7fe5dc1097de55d9b434c8f0b132e6825753765d
soltshio <shanshanshien@gmail.com>
2026-08-25 21:12:57 +0900

    魔法陣展開時に使用するボタンを作成

---
+++ MagicWand/Assets/Scripts/General/AnimatorProperty/DeployMagicCircleUIAnimatorProperty.cs
@@ -0,0 +1,10 @@
+﻿//作成者:杉山
+//魔法陣展開のUIのアニメーションのプロパティ
+
+public class DeployMagicCircleUIAnimatorProperty
+{
+    //表示のトリガー
+    public static string ShowTriggerName = "Show";
+    //非表示のトリガー
+    public static string HideTriggerName = "Hide";
+}
---
+++ MagicWand/Assets/Scripts/UI/HoverAutoClickButton/ViewUI/ButtonHoveringViewUI_DeployMagicCircleUI.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+//作成者:杉山
+//数秒間カーソルを合わせていると、自動クリックするボタンがホバー中(カーソルが合わせられている時)にホバー時間の割合をゲージで表示する
+//ImageのScaleを操作するタイプ
+
+public class ButtonHoveringViewUI_DeployMagicCircleUI : MonoBehaviour
+{
+    [SerializeField]
+    HoverAutoClickButton _hoverAutoClickButton;
+
+    [SerializeField]
+    Image _hoverProgressGauge;
+
+    [SerializeField]
+    Animator _hoverAutoClickButtonAnimator;
+
+    void OnEnable()
+    {
+        _hoverProgressGauge.transform.localScale = new Vector3(0f, 0f, 0f);
+
+        _hoverAutoClickButton.OnStateChanged += OnButtonStateChanged;
+    }
+
+    void OnDisable()
+    {
+        _hoverAutoClickButton.OnStateChanged -= OnButtonStateChanged;
+    }
+
+    void Update()
+    {
+        if (_hoverAutoClickButton.CurrentState != HoverAutoClickButtonEState.Hovering) return;
+
+        float gaugeAmount = _hoverAutoClickButton.HoveringTime / _hoverAutoClickButton.Parameter.HoverDurationToClick;
+
+        _hoverProgressGauge.transform.localScale = new Vector3(gaugeAmount, gaugeAmount, gaugeAmount);
+    }
+
+    void OnButtonStateChanged(HoverAutoClickButtonEState state)
+    {
+        if(state == HoverAutoClickButtonEState.Clicked)
+        {
+            //だんだんUIが消えていくようにする
+            _hoverAutoClickButtonAnimator.SetTrigger(DeployMagicCircleUIAnimatorProperty.HideTriggerName);
+        }
+    }
+}
