9c361958247677aa9f3351ac6d4bc15da28276ef
soltshio <shanshanshien@gmail.com>
2026-08-26 17:25:38 +0900

    一度離れてから再度触った際にエフェクトのトレイルが途切れるようにするコードを作成

---
+++ MagicWand/Assets/Scripts/Effect/CursorEffect/CursorTrailEffectController.cs
@@ -0,0 +1,29 @@
+﻿using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+//作成者:杉山
+//カーソルのトレイルエフェクトの制御をする
+//杖が一度離れてから触れた際にトレイルが途切れるようにすること
+
+public class CursorTrailEffectController : MonoBehaviour
+{
+    [SerializeField]
+    ParticleSystem _trailEffect;
+
+    HokuyoDataReceiver _hokuyoDataReceiver;
+
+    async void OnEnable()
+    {
+        _hokuyoDataReceiver = await HokuyoDataReceiver.GetInstanceAsync(this.GetCancellationTokenOnDestroy());
+
+        _hokuyoDataReceiver.OnSwitchIsExistObject += ResetEffectOnEnterObjectInHokuyoDetect;
+    }
+
+    void ResetEffectOnEnterObjectInHokuyoDetect(bool isExistObject)
+    {
+        if (!isExistObject) return;
+
+        _trailEffect.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+        _trailEffect.Play();
+    }
+}
