7d8a179edd9b76bcb17c7c17940b904ffc610ba3
soltshio <shanshanshien@gmail.com>
2026-09-07 14:50:23 +0900

    とりあえず仮で魔法発動時にエフェクトを表示するようにした

--- MagicWand/Assets/Scripts/MagicCircleClose/InvokedMagicIconEffect.cs
+++ MagicWand/Assets/Scripts/MagicCircleClose/InvokedMagicIconEffect.cs
@@ -1,16 +1,52 @@
+﻿using Cysharp.Threading.Tasks;
+using System;
+using System.Threading.Tasks;
 using UnityEngine;
+
+//作成者:杉山
+//発動した魔法のアイコンの制御
 
 public class InvokedMagicIconEffect : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField]
+    MagicList _magicList;
+
+    [SerializeField]
+    Animator _iconAnimator;
+
+    [SerializeField]
+    SpriteRenderer _iconRenderer;
+
+    [SerializeField]
+    float _activeDuration=5f;
+
+    public async UniTask PlayAsync(EMagic invokedMagic)
     {
-        
+        var ct = this.GetCancellationTokenOnDestroy();
+
+        //アイコンの色を変えておく
+        SetIconColor(invokedMagic);
+
+        _iconAnimator.gameObject.SetActive(true);
+
+        await UniTask.Delay(TimeSpan.FromSeconds(_activeDuration), cancellationToken:ct);
+
+        _iconAnimator.gameObject.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    void SetIconColor(EMagic invokedMagic)
     {
-        
+        //アイコンの色やテクスチャなどを取得
+        var invokedMagicIconEffectProperty = _magicList.GetComponentFromMagic<InvokedMagicIconEffectProperty>(invokedMagic);
+
+        if (invokedMagicIconEffectProperty == null) return;
+
+        _iconRenderer.color = invokedMagicIconEffectProperty.IconColor;
+        _iconRenderer.sprite = invokedMagicIconEffectProperty.IconSprite;
+    }
+
+    void Start()
+    {
+        _iconAnimator.gameObject.SetActive(false);
     }
 }
--- MagicWand/Assets/Scripts/MagicCircleClose/MagicCircleCloseManager.cs
+++ MagicWand/Assets/Scripts/MagicCircleClose/MagicCircleCloseManager.cs
@@ -1,4 +1,5 @@
 ﻿using Cysharp.Threading.Tasks;
+using System;
 using UnityEngine;
 
 //作成者:杉山
@@ -12,13 +13,21 @@ public class MagicCircleCloseManager : MonoBehaviour
     [Tooltip("魔法陣の表示・非表示をする機能")] [SerializeField]
     MagicCircleActiveHandler _magicCircleActiveHandler;
 
+    [SerializeField]
+    InvokedMagicIconEffect _invokedMagicIconEffect;
+
+    [SerializeField]
+    float _delayDurationFromInvokedMagicIconEffectFromHideMagicCircle = 1f;
+
     public async UniTask CloseAsync(EMagic invokedMagic)
     {
         var token = this.GetCancellationTokenOnDestroy();
 
         //発動した魔法のアイコンを表示する
+        _invokedMagicIconEffect.PlayAsync(invokedMagic).Forget();
 
         //少し遅らせる
+        await UniTask.Delay(TimeSpan.FromSeconds(_delayDurationFromInvokedMagicIconEffectFromHideMagicCircle), cancellationToken: token);
 
         //魔法陣の線を目立たせる
         _magicSphereTrail.Activate();
